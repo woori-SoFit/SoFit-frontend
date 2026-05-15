d56aeb9e97fa5ce93aeabe059b5cf8faa03c0f0e
light11014 <dtq1014@naver.com>
2026-05-15 15:18:25 +0900

    [SOFIT-60] Style: 사이드바 메뉴 그룹 간격 확대

--- admin-front/src/components/common/Sidebar.tsx
+++ admin-front/src/components/common/Sidebar.tsx
@@ -53,7 +53,7 @@ export function Sidebar() {
       {/* 메뉴 그룹 */}
       <nav className="flex-1 px-3">
         {MENU_GROUPS.map((group) => (
-          <div key={group.category} className="mb-4">
+          <div key={group.category} className="mb-6">
             <p className="px-2 mb-1 text-xs font-semibold text-text-disabled uppercase">
               {group.category}
             </p>
